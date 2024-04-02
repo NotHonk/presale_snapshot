c8921e566129506b1615ce5592a67bd64b3be11e
HONK <165743738+NotHonk@users.noreply.github.com>
2024-04-02 11:06:48 +0800

    Add index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require("fs").promises;
+const api =
+  "https://api.tonraffles.app/api/v1/fairlaunch/purchases?address=EQDu9ZUCPWK5wgxiW-eiWuJZKANQMhKOPrUS4ZrQO5MUdOx-&limit=2000&offset=0";
+const path = "./snapshot.csv";
+
+async function snapshot() {
+  try {
+    const response = await fetch(api);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const body = await response.json();
+    const list = body["data"].filter((x) => x["total_amount"] >= 5000000000);
+
+    const csv = list
+      .map((x) => `${x["owner"]},${x["total_amount"]}`)
+      .join("\n");
+
+    await fs.writeFile(path, csv);
+    console.log("Snapshot saved successfully.");
+  } catch (e) {
+    console.error("An error occurred:", e);
+  }
+}
+
+snapshot();
